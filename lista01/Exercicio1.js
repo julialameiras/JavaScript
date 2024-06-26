7b120037bc98758f383fe0cb39672efaa24c30b7
julialameiras <julialameiras@gmail.com>
2024-06-26 14:26:43 -0300

    Exercícios 1 e 2

---
+++ lista01/Exercicio1.js
@@ -0,0 +1,5 @@
+let salario = 10000;
+let abono = 1000;
+
+
+console.log("\nO novo salário é: ",salario+abono);
---
+++ lista01/Exercicio2.js
@@ -0,0 +1,9 @@
+let primeiraNota = 10;
+let segundaNota = 8;
+let terceiraNota = 7;
+let quartaNota = 7.5;
+let notaFinal;
+
+notaFinal = (primeiraNota + segundaNota + terceiraNota + quartaNota) /4;
+console.log("\nA média das notas é: " + notaFinal.toFixed(1));
+
